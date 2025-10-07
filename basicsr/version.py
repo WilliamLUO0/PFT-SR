1c022515d70bf6b19125b8b54aa8c5a2298b53d5
WilliamLUO0 <williamlzh00@gmail.com>
2025-10-07 16:57:43 +1300

    remove basicsr package

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Tue Aug 26 12:56:57 2025
+# TIME: Tue Oct  7 16:53:24 2025
 __version__ = '0.1.1'
-__gitsha__ = 'f2c0f8e'
+__gitsha__ = '71e93de'
 version_info = (0, 1, 1)
